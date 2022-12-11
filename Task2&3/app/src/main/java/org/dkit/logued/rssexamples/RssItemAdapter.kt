9a0a019a794facde3851546956551bbc35d77785
jeremysoh0210 <jeremysoh222@gmail.com>
2022-12-11 13:42:00 +0000

    task4&5

--- Task2&3/app/src/main/java/org/dkit/logued/rssexamples/RssItemAdapter.kt
+++ Task2&3/app/src/main/java/org/dkit/logued/rssexamples/RssItemAdapter.kt
@@ -1,7 +1,5 @@
 package org.dkit.logued.rssexamples
 
-import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
